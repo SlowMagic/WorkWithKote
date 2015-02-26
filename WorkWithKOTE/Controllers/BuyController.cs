58d6d2fb4fce5b9a3cf2996b0a57fa4c09d0da16
Evgen <Levitskiyorange@gmail.com>
2015-02-26 12:04:19 +0200

    new

---
+++ WorkWithKOTE/Controllers/BuyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkWithKOTE.Models;
+namespace WorkWithKOTE.Controllers
+{
+
+    public class BuyController : Controller
+    {
+        //
+        // GET: /Buy/
+        TourContext db = new TourContext();
+        public ActionResult Index(int id )
+        {
+            Tour date = db.Tour.Find(id);
+            ViewBag.TourPrices = date.Cost;
+            Trip trip = new Trip();
+            trip.TourPrice = date.Cost;
+            ViewBag.DateTourId = new SelectList(db.DateTours.Where(m => m.TourId == id), "DateTourId", "FirstDate");
+            return View(trip);
+        }
+
+    }
+}
--- WorkWithKOTE/Models/TourModels.cs
+++ WorkWithKOTE/Models/TourModels.cs
@@ -16,14 +16,35 @@ namespace WorkWithKOTE.Models
     {
 
     }
-
+        public DbSet<Trip> Trip { get; set; }
         public DbSet<Tour> Tour { get; set; }
-
         public DbSet<DateTour> DateTours { get; set; }
         public DbSet<Teg> Teg { get; set; }
         public DbSet<DopUslug> DopUslugs { get; set; }
         public DbSet<Gallery> Gallery { get; set; }
         public DbSet<RoutePoint> RoutePoint { get; set; }
+    }
+    [Table("Trip")]
+    public class Trip
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int TripID { get; set; }
+        public decimal? TourPrice { get; set; }
+        public int AmtPeople { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FatherName { get; set; }
+        public string Sex { get; set; }
+        public string BirthDay { get; set; }
+        public string Citizenship { get; set; }
+        public string Pasport { get; set; }
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Номер введён неверно.")]
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public int DateTourId { get; set; }
+        public int TourId { get; set; }
+        public string Valuta { get; set; }
     }
     [Table ("Tour")]
    public class Tour
@@ -35,15 +56,15 @@ namespace WorkWithKOTE.Models
        public string TitleTour { get; set; }
        [AllowHtml]
        public string DiscriptionTour { get; set; }
-       public float? Cost { get; set; }
+       public decimal? Cost { get; set; }
        public string Valuta { get; set; }
-       public string PrePay { get; set; }
+       public decimal? PrePay { get; set; }
        public string Reservation { get; set; }
        public bool IsBus { get; set; }
        public bool IsAriplane { get; set; }
        public bool IsShip { get; set; }
        public string PodpicePrice { get; set; }
-       public string AukcionPrice { get; set; }
+       public decimal? AukcionPrice { get; set; }
        public string KommisiaAgent { get; set; } 
        public string SuppName { get; set; }
        public string SuppVkontakte { get; set; }
@@ -57,7 +78,7 @@ namespace WorkWithKOTE.Models
        public string Document { get; set; }
        public float? Bonus { get; set; }
        public IList<Teg> Teg { get; set; }
-       public List<RoutePoint> RoutePoints { get; set; }
+       public IList<RoutePoint> RoutePoints { get; set; }
        public IList<DateTour> DateTour { get; set; }
        public IList<DopUslug> DopUslug { get; set; }
        public int? GalleryID { get; set; }
